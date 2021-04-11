e9f6eb1703095d26099877c501edef2df76dfed1
Shoaib Ihsan <theshoaibihsan10@gmail.com>
2021-04-11 11:05:23 +0500

    asdoadj

--- application/main_app/routes.py
+++ application/main_app/routes.py
@@ -14,7 +14,7 @@ main = Blueprint('main', __name__,static_folder='../static')
 @main.route('/get_all_categories_for_normal_users')
 def GetAllCategoriesForNormalUsers():
     user_default_location = request.args.get('user_default_location')
-    get_categories_sql = text("SELECT *,(SELECT count(*) FROM vendors WHERE  vendor_zip_code='"+str(user_default_location)+"' OR city_name LIKE '%"+str(user_default_location)+"%' AND vendor_category=vendor_category_id ) as home_page_vendors_count  from vendor__categories")
+    get_categories_sql = text("SELECT *,(SELECT count(*) FROM vendors WHERE  vendor_zip_code='"+str(user_default_location)+"' OR city_name ='"+str(user_default_location)+"' AND vendor_category=vendor_category_id ) as home_page_vendors_count  from vendor__categories")
     get_categories_query = db.engine.execute(get_categories_sql)
     vendor_categories_schema = Vendor_CategoriesSchema(many=True)
     all_categories =vendor_categories_schema.dump(get_categories_query)
@@ -25,7 +25,7 @@ def view_category_by_user_default_location():
     category_id = request.args.get('category_id')
     user_default_location = request.args.get('user_default_location')
     
-    vendors_sql = text("Select * from vendors WHERE  vendor_zip_code='"+str(user_default_location)+"' OR city_name LIKE '%"+str(user_default_location)+"%' AND vendor_category="+str(category_id)+"")
+    vendors_sql = text("Select * from vendors WHERE  vendor_zip_code='"+str(user_default_location)+"' OR city_name ='"+str(user_default_location)+"' AND vendor_category="+str(category_id)+"")
 
     
 
@@ -35,7 +35,7 @@ def view_category_by_user_default_location():
 
 
 
-    category_sql = text("Select *,(SELECT count(*) FROM vendors WHERE  vendor_zip_code='"+str(user_default_location)+"' OR city_name LIKE '%"+str(user_default_location)+"%' AND vendor_category=vendor_category_id ) as vendors_count from vendor__categories where vendor_category_id="+str(category_id)+"")
+    category_sql = text("Select *,(SELECT count(*) FROM vendors WHERE  vendor_zip_code='"+str(user_default_location)+"' OR city_name ='"+str(user_default_location)+"' AND vendor_category=vendor_category_id ) as vendors_count from vendor__categories where vendor_category_id="+str(category_id)+"")
     category_query = db.engine.execute(category_sql)
     category_schema = Vendor_CategoriesSchema(many=True)
     category_info = category_schema.dump(category_query)
@@ -77,7 +77,7 @@ def ViewSearchedCategory():
     location = request.args.get('location')
     category_id = request.args.get('category_id')
 
-    vendors_sql = text("Select * from vendors WHERE  vendor_zip_code='"+str(location)+"' OR city_name LIKE '%"+str(location)+"%' AND vendor_category="+str(category_id)+"")
+    vendors_sql = text("Select * from vendors WHERE  vendor_zip_code='"+str(location)+"' OR city_name='"+str(location)+"' AND vendor_category="+str(category_id)+"")
 
     
 
@@ -87,7 +87,7 @@ def ViewSearchedCategory():
 
 
 
-    category_sql = text("Select *,(SELECT count(*) FROM vendors WHERE  vendor_zip_code='"+str(location)+"' OR city_name LIKE '%"+str(location)+"%' AND vendor_category=vendor_category_id ) as vendors_count from vendor__categories where vendor_category_id="+str(category_id)+"")
+    category_sql = text("Select *,(SELECT count(*) FROM vendors WHERE  vendor_zip_code='"+str(location)+"' OR city_name ='"+str(location)+"' AND vendor_category=vendor_category_id ) as vendors_count from vendor__categories where vendor_category_id="+str(category_id)+"")
     category_query = db.engine.execute(category_sql)
     category_schema = Vendor_CategoriesSchema(many=True)
     category_info = category_schema.dump(category_query)
@@ -100,7 +100,7 @@ def dropdown_vendors():
     location = request.args.get('location')
     category_id = request.args.get('category_id')
 
-    vendors_sql = text("Select * from vendors WHERE  vendor_zip_code='"+str(location)+"' OR city_name LIKE '%"+str(location)+"%' AND vendor_category="+str(category_id)+" LIMIT 0, 5")
+    vendors_sql = text("Select * from vendors WHERE  vendor_zip_code='"+str(location)+"' OR city_name ='"+str(location)+"' AND vendor_category="+str(category_id)+" LIMIT 0, 5")
 
     
 
