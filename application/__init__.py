42a3abe0a39ae2741975c9811542de3a1cf23516
Shoaib Ihsan <theshoaibihsan10@gmail.com>
2021-04-26 13:09:42 +0500

    asd

--- application/__init__.py
+++ application/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 Migrate(app,db)
 
