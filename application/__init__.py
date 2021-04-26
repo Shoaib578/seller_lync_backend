9446313b51d91ca9b072a67eeb8b85e1e3133d03
Shoaib Ihsan <theshoaibihsan10@gmail.com>
2021-04-26 13:41:36 +0500

    asd

--- application/__init__.py
+++ application/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask,Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
+
 from flask_cors import CORS
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'assdasdaiahsidha8s6e82ugaugsd'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL?sslmode=require')
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///D:\Projects\React Native Projects\SellerLync\APIS\db.sqlite"
 
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app,db)
 
