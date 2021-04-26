598f012e7785733188699fb694ca176b1910c27c
Shoaib Ihsan <theshoaibihsan10@gmail.com>
2021-04-26 11:36:32 +0500

    asd

--- create_db.py
+++ create_db.py
@@ -1,2 +1,16 @@
 from application import db
+from application.admin_panel.models import Users
+
+from werkzeug.security import generate_password_hash,check_password_hash
 db.create_all()
+
+
+admin_user = Users.query.filter_by(is_admin=1)
+if admin_user.count() == 0:
+    psw = generate_password_hash('admin2589')
+    admin = Users(name='Admin',email='theadmin287@gmail.com',password=psw,phone_no='0333434',is_admin=1,user_cityname='india')
+    db.session.add(admin)
+    db.session.commit()
+else:
+    pass
+
