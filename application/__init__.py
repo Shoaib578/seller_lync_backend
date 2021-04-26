b664fa4191b9f129d67aa662dd867e7896eea99d
Shoaib Ihsan <theshoaibihsan10@gmail.com>
2021-04-26 13:33:24 +0500

    asd

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'assdasdaiahsidha8s6e82ugaugsd'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dmrqejlvdfecit:b76cf94be74ad2c20b6e93297172f68d31caf12ebbe261f183c71b5c1c9a5785@ec2-3-233-43-103.compute-1.amazonaws.com:5432/d80fnc1h5ocupb'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL?sslmode=require')
 
 
 db = SQLAlchemy(app)
