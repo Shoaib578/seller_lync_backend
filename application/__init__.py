5a74496ab9e93c6697ee2347279b90d229e6b915
Shoaib Ihsan <theshoaibihsan10@gmail.com>
2021-04-27 15:03:03 +0500

    asd

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://dtcgzzurvhmyok:8ce9d739907569d618ebfe80185834d85268479b01ac47c41632c6e12854166a@ec2-54-87-112-29.compute-1.amazonaws.com:5432/dbbelahmo011t8"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///D:\Projects\React Native Projects\SellerLync\APIS\db.sqlite"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
